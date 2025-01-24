d8c386e85cd0285b19b544eb254d03159d5454b2
zrei <88964793+zrei@users.noreply.github.com>
2025-01-24 20:54:49 +0800

    Organisation + Base Scripts

---
+++ Assets/Scripts/Base/GlobalEvents.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public delegate void IntEvent(int _);
+public delegate void VoidEvent();
+public delegate void FloatEvent(float _);
+public delegate void Vector3Event(Vector3 _);
+
+public static class GlobalEvents 
+{
+  
+}
