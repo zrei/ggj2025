e71ef55152473b80f99d1548befb4cd4c6262b95
zrei <88964793+zrei@users.noreply.github.com>
2025-01-26 01:05:10 +0800

    misc

--- Assets/Scripts/Gameplay/Enemy/EnemyUnit.cs
+++ Assets/Scripts/Gameplay/Enemy/EnemyUnit.cs
@@ -90,7 +90,6 @@ public class EnemyUnit : MonoBehaviour, IDamagable
             if (m_CurrDamageCooldown > 0)
                 return;
 
-            // TODO: Replace with exposed player velocity
             float playerSpeed = PlayerBeam.Instance.SpeedMagnitude;
             if (playerSpeed > GlobalEvents.Player.MinimumSpeedForDamage)
             {
