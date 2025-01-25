59b7f4afdb6da5c59a778d49dd40afea57ddf8f8
knotstoks <70320812+knotstoks@users.noreply.github.com>
2025-01-25 18:00:08 +0800

    Update EnemyProjectile.cs

--- Assets/Scripts/Gameplay/EnemyProjectile.cs
+++ Assets/Scripts/Gameplay/EnemyProjectile.cs
@@ -4,12 +4,44 @@ using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour
 {
+    private Rigidbody2D m_Rigidbody;
+    private Vector2 m_Direction;
+    private int m_Attack;
+    private float m_Speed;
+
+    private bool m_IsUsed;
     /*
      * Called by enemy behaviour
      */
-    public void Setup(Vector2 direction)
+    public void Setup(Vector2 direction, int attack, float speed)
     {
+        m_Direction = direction;
+        m_Attack = attack;
+        m_Speed = speed;
+        m_IsUsed = false;
+    }
 
+    private void Update()
+    {
+        if (!m_IsUsed)
+        {
+            m_Rigidbody.velocity = m_Direction * m_Speed;
+        }
+        // TODO: Dirty all that it touches
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (m_IsUsed)
+        {
+            return;
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            new DamageInfo(m_Attack, DamageType.HealthDec, Player.Instance).ProcessDamage();
+            m_IsUsed = true;
+        }
     }
 
     public void Despawn()
