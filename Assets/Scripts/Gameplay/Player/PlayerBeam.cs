fe1e1da654ee2fcb671a14ef5cdc63b3b6423176
knotstoks <70320812+knotstoks@users.noreply.github.com>
2025-01-26 15:21:34 +0800

    Update PlayerBeam.cs

--- Assets/Scripts/Gameplay/Player/PlayerBeam.cs
+++ Assets/Scripts/Gameplay/Player/PlayerBeam.cs
@@ -75,7 +75,7 @@ public class PlayerBeam : Singleton<PlayerBeam>
         m_BubbleListLength = BubbleParticles.Count;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         GetPlayerInputs();
         UpdateAim();
